c91de8de1d24f945cab3ed40ef0debf168a97dd9
Hugo Brandao <hugo@feelrobotics.com>
2020-01-16 22:50:04 +0100

    update workload and job classes

--- datasim/workloads.py
+++ datasim/workloads.py
@@ -6,7 +6,11 @@ from helpers import random_uniform
 class Job:
     def __init__(self, job_id, job_size):
         self.id = job_id
+        self.status = STATUS_NEW
         self.size = job_size
+        self.stats = {}
+        self.action = 'AUTH'
+        self.respond_method = None
 
 class Workload(Process):
     def __init__(self, env, components, wl_params):
@@ -19,6 +23,7 @@ class Workload(Process):
         self.start_time = self.wl_params['start_time']
         self.end_time = self.wl_params['end_time']
         self.target_name = self.wl_params['target']['name']
+        self.js_params = wl_params['job_size']
         self.ia_params = wl_params['interarrival']
         self.vl_params = wl_params['volume']
         Process(self.env,self.generate())
@@ -27,42 +32,26 @@ class Workload(Process):
             # interarrival time
             interarrival = int(random_uniform(self.ia_params)) #TODO: get specified interarrival distribution
             yield self.env.timeout(interarrival)
-            
             for i in range(int(random_uniform(self.vl_params))):
                 try:
                     # create new job in the workload
                     last_job_id = self.last_job_id + 1 if self.last_job_id is not None else 0
                     job_name = self.name + "_" + str(last_job_id)
-                    job = Job(job_name,10)
+                    job_size = int(random_uniform(self.js_params))
+                    job = Job(job_name,job_size)
 
                     # send job to target component
                     target = self.components[self.target_name]
-                    self.env.process(target.enqueue(job))
+                    self.env.process(target.authenticate_user(job))
                     self.last_job_id = last_job_id
                     print(f'[workload] Job {job.id} sent to {target.name} at {self.env.now}')
                 except Exception as e:
                     print(f'[workload] Error generating job {last_job_id} from workload {self.name} at {self.env.now}: {str(e)}')
 
 
-# All the code below is gonna be discarted soon
-def db_request_generator(env, wl_params, components):
-    for i in itertools.count():
-        # generate interval according to stochastic distribution
-        interval = random_number(wl_params)
-        # print('%s wating to request read_write operation at %d.' % (wl_params['origin'],env.now))
-        yield env.timeout(interval)
-        # print('%s starting request read_write operation at %d.' % (wl_params['origin'],env.now))
-        env.process(components[wl_params['target']['name']].read_write(wl_params['origin'], wl_params['request_size']))
-        # print('%s finished request read_write operation at %d.' % (wl_params['origin'],env.now))
-
-def get_workload(env, wl_params, components):
-    workload = dict(
-        db_request=db_request_generator
-        )
-    return env.process(workload[wl_params['type']](env, wl_params, components))
     
-def parse_workloads(env, workloads_list, components):
+def parse_workloads(env, components, workloads_list):
     workloads = {}
     for wl_params in workloads_list:
-        workloads[wl_params['origin']] = get_workload(env, wl_params, components)
+        workloads[wl_params['name']] = Workload(env, components, wl_params)
     return workloads
