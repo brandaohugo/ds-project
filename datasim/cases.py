cf833af13ac6d0d61f15ff5f598999698d2425b8
Hugo Brandao <hugo@feelrobotics.com>
2020-01-16 22:47:30 +0100

    Update case config

--- datasim/cases.py
+++ datasim/cases.py
@@ -1,85 +1,47 @@
 sim_params_1 = {
     'settings': {
-            'sim_time' : 50,
+            'sim_time' : 10,
     },
     'components' : [
         {
-            'type': 'server',
-            'name' : 'db_server',
-            'recovery_time' : 0, 
-            'actions' : {
-                    'read_write' : {
-                        'proc_speed': 1
-                    },
-            },
-            'resource': {
-                    'name' : 'database',
-                    'capacity' : 2
-                } 
+            'type': 'auth_server',
+            'db_server_name': 'auth_db_server',
+            'name' : 'auth_server',
+            'num_cores': 4,
+            'core_speed': 1
         },
         {
-            'type': 'server',
-            'name' : 'load_balancer',
-            'recovery_time' : 0, 
-            'actions' : {
-                    'read_write' : {
-                        'proc_speed': 1
-                    },
-            },
-            'resource': {
-                    'name' : 'database',
-                    'capacity' : 2
-                } 
-
-        },
-        {
-            'type': 'server',
-            'name' : 'load_balancer2',
-            'recovery_time' : 0,
-            'actions' : {
-                    'read_write' : {
-                        'proc_speed': 1
-                    },
-            },
-            'resource': {
-                    'name' : 'database',
-                    'capacity' : 2
-                },
+            'type': 'db_server',
+            'name': 'auth_db_server',
+            'num_cores': 4,
+            'core_speed': 1
         }
     ],
     'workloads' : [
         {
+            'start_time': 0,
+            'end_time': 6,
             'type': 'db_request',
-            'origin': 'user_1',
+            'name': 'user_authentication',
+            'request_size': 1,
             'target': {
-                'name': 'db_server',
+                'name': 'auth_server',
             },
-            'request_size': 10,
-            'distribution': 'uniform',
-            'low': 1,
-            'high': 5
-        },
-        {
-            'type': 'db_request',
-            'origin': 'user_2',
-            'target': {
-                'name': 'load_balancer',
+            'job_size' : {
+                'distribution': 'uniform',
+                'low': 1,
+                'high': 1
             },
-            'request_size': 12,
-            'distribution': 'uniform',
-            'low': 6,
-            'high': 7
-        },
-        {
-            'type': 'db_request',
-            'origin': 'user_3',
-            'target': {
-                'name': 'load_balancer2',
+            'interarrival': {
+                'distribution': 'uniform',
+                'low': 1,
+                'high': 1,
             },
-            'request_size': 12,
-            'distribution': 'uniform',
-            'low': 6,
-            'high': 7
+            'volume': {
+                'distribution': 'uniform',
+                'low': 1,
+                'high': 1
+            }
         }
     ],
 }
