eca9a91e3dcb0be72ca85b512fcd9d338ebec493
samkellerhals <kellerhalssamuel@gmail.com>
2020-01-18 18:46:20 +0100

    add parameter viewer

---
+++ datasim/static/js/parameters.js
@@ -0,0 +1,58 @@
+var simulation_params = {
+    'settings': {
+        'sim_time': 40,
+    },
+    'components': [{
+            'type': 'load_balancer',
+            'name': 'auth_load_balancer',
+            'servers': ['auth_server_01', 'auth_server_02'],
+            'num_cores': 4,
+            'core_speed': 1
+        },
+        {
+            'type': 'auth_server',
+            'db_server_name': 'auth_db_server',
+            'name': 'auth_server_01',
+            'num_cores': 4,
+            'core_speed': 1
+        },
+        {
+            'type': 'auth_server',
+            'db_server_name': 'auth_db_server',
+            'name': 'auth_server_02',
+            'num_cores': 4,
+            'core_speed': 1
+        },
+        {
+            'type': 'db_server',
+            'name': 'auth_db_server',
+            'num_cores': 4,
+            'core_speed': 1
+        }
+    ],
+    'workloads': [{
+        'start_time': 0,
+        'end_time': 6,
+        'type': 'db_request',
+        'name': 'user_authentication',
+        'request_size': 1,
+        'target': {
+            'name': 'auth_load_balancer',
+        },
+        'job_size': {
+            'distribution': 'uniform',
+            'low': 1,
+            'high': 1
+        },
+        'interarrival': {
+            'distribution': 'uniform',
+            'low': 1,
+            'high': 1,
+        },
+        'volume': {
+            'distribution': 'uniform',
+            'low': 1,
+            'high': 1
+        }
+    }],
+}
