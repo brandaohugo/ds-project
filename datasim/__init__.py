cfead0ac3a1be6f03806d7c378c881a8b442d0e9
samkellerhals <kellerhalssamuel@gmail.com>
2020-01-19 20:57:20 +0100

    add visualisation (need to fix bug)

--- datasim/__init__.py
+++ datasim/__init__.py
@@ -30,22 +30,31 @@ def create_app(test_config=None):
 
     app.register_blueprint(auth.bp)
 
-    '''TODO: plot resource usage 
-    @app.route('/eventplot.png')
-    def make_event_plot():
-        fig = processor.create_event_figure()
-        output = io.BytesIO()
-        FigureCanvas(fig).print_png(output)
-        return Response(output.getvalue(), mimetype='image/png')
+    def bytes_todf(file):
+        bytes_data = file.read()
+        s=str(bytes_data,'utf-8')
+        data = StringIO(s) 
+        df=pd.read_csv(data)
+        return df
+        
+    def random_hex():
+        random_number = random.randint(0,16777215)
+        hex_number = str(hex(random_number))
+        hex_number ='#'+ hex_number[2:]
+        return str(hex_number)
 
-    @app.route('/queueplot.png')
-    def make_queue_plot():
-        fig = processor.create_queue_figure()
-        output = io.BytesIO()
-        FigureCanvas(fig).print_png(output)
-        return Response(output.getvalue(), mimetype='image/png')
-    '''
-
+    def create_dict_list(df, y_attribute):
+        lst = []
+        for name in df.name.unique():
+            dictionary = dict(
+                data=list(df[df['name']==name][y_attribute].values), 
+                label=name,
+                borderColor=random_hex(),
+                fill='false'
+            )
+            lst.append(dictionary)
+        return lst
+    
     @app.route('/eventcount.png')
     def get_event_count_fig():
         #TODO: enable user to select different sim runs for plotting
@@ -62,15 +71,7 @@ def create_app(test_config=None):
             return True
         else:
             return False
-
-    def bytes_todf(file):
-        bytes_data = file.read()
-        s=str(bytes_data,'utf-8')
-        data = StringIO(s) 
-        df=pd.read_csv(data)
-        return df
-        
-    # web application routes
+    
     @app.route("/")
     @auth.login_required
     def index():
@@ -96,19 +97,22 @@ def create_app(test_config=None):
                 return redirect(request.url)
             
             if file and allowed_file(file.filename):
+                
                 # Process upload 
                 df = bytes_todf(file)
-                event_data = processor.extract_events_count(df)
-                res_legend = 'Resource Queue'
-                res_values = event_data['count']
-                res_labels = event_data['type']
-                return render_template("index.html", section='visualiser', res_values=res_values, res_labels=res_labels)
+                dict_list = create_dict_list(df, 'used_cores')
+                sim_time = [i for i in range(len(dict_list[0]['data']))]
+                
+                return render_template("index.html", section='visualiser', data=dict_list, sim_time=sim_time)
 
             # other functions used previously to parse data:    
             # res_labels, res_values = processor.prepare_plot('datasim/log/res_20200115185434.
             # event_data = processor.extract_events_count(pd.read_csv(log_filename))
+        
+        sim_time = [0]
+        data = [0]
             
-        return render_template("index.html", section='visualiser')
+        return render_template("index.html", section='visualiser', data=data, sim_time=sim_time) 
 
     @app.route("/parameters")
     @auth.login_required
---
+++ datasim/templates/chart_test.js
@@ -0,0 +1,56 @@
+var MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+var config = {
+    type: 'line',
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: window.chartColors.red,
+            borderColor: window.chartColors.red,
+            data: [
+                // FILL IN DATA HERE
+            ],
+            fill: false,
+        }, {
+            label: 'My Second dataset',
+            fill: false,
+            backgroundColor: window.chartColors.blue,
+            borderColor: window.chartColors.blue,
+            data: [
+                // FILL IN DATA HERE
+            ],
+        }]
+    },
+    options: {
+        responsive: true,
+        title: {
+            display: true,
+            text: 'Chart.js Line Chart'
+        },
+        tooltips: {
+            mode: 'index',
+            intersect: false,
+        },
+        hover: {
+            mode: 'nearest',
+            intersect: true
+        },
+        scales: {
+            xAxes: [{
+                display: true,
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Month'
+                }
+            }],
+            yAxes: [{
+                display: true,
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Value'
+                }
+            }]
+        }
+    }
+};
